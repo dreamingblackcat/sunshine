78ace855981d98827ef00434bbdb6ff8371d9275
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-03-04 15:57:31 +0630

    lesson 3 step 7 => creatign an implicit intent for map apps via a menu option

--- app/src/main/java/com/example/dbc/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/dbc/sunshine/app/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.dbc.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,10 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        if( id == R.id.action_map){
+            openPreferredLocationInMap();
+            return true;
+        }
         if (id == R.id.action_settings) {
             startActivity(new Intent(this,SettingsActivity.class));
             return true;
@@ -44,5 +53,21 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void openPreferredLocationInMap(){
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String location  = sharedPref.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                          .appendQueryParameter("q",location)
+                          .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else{
+            Log.e(LOG_TAG,"Could not find : " + location + "no map");
+        }
+    }
 
 }
