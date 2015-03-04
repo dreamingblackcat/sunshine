3a41abba64686d94793027a7bbc84217bca73d78
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-03-04 14:41:13 +0630

    lesson 3 step 5 => creating an EditTextPreference for location and use that to fetch data for that location

--- app/src/main/java/com/example/dbc/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/dbc/sunshine/app/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.dbc.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -47,7 +49,10 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if(id == R.id.action_refresh){
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = sharedPref.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
