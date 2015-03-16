981bb5207de2fe05dd221e40265bb8e5bccefdb1
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-03-16 23:51:35 +0630

    lesson 4a finished. lesson 4b mid

---
+++ app/src/main/java/com/example/dbc/sunshine/app/data/WeatherContract.java
@@ -0,0 +1,98 @@
+package com.example.dbc.sunshine.app.data;
+
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.text.format.Time;
+
+public class WeatherContract {
+
+    // To make it easy to query for the exact date, we normalize all dates that go into
+    // the database to the start of the the Julian day at UTC.
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.setToNow();
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+
+
+    public static final String CONTENT_AUTHORITY = "com.example.dbc.sunshine.app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_WEATHER = "weather";
+    public static final String PATH_LOCATION = "location";
+
+    public static final class LocationEntry implements BaseColumns{
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION ;
+        public static final String CONTENT_iTEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION ;
+
+        public static final String TABLE_NAME = "location";
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+        public static final String COLUMN_CITY_NAME = "city_name";
+        public static final String COLUMN_COORD_LAT = "location_lat";
+        public static final String COLUMN_COORD_LONG = "location_long";
+
+        public static Uri buildLocationUri(long id){
+           return ContentUris.withAppendedId(CONTENT_URI,id);
+        }
+    }
+
+
+    public static final class WeatherEntry implements BaseColumns{
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER ;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER ;
+
+
+        public static final String TABLE_NAME = "weather";
+        public static final String COLUMN_LOC_KEY = "location_id";
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_WEATHER_ID = "weather_id";
+        public static final String COLUMN_SHORT_DESC = "short_desc";
+        public static final String COLUMN_MIN_TEMP = "min";
+        public static final String COLUMN_MAX_TEMP = "max";
+        public static final String COLUMN_HUMIDITY = "humidity";
+        public static final String COLUMN_PRESSURE = "pressure";
+        public static final String COLUMN_WIND_SPEED = "wind";
+        public static final String COLUMN_DEGREES = "degrees";
+
+        public static Uri buildWeatherUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI,id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(String location_setting,long startDate){
+
+            long normalizedDate = normalizeDate(startDate);
+            return CONTENT_URI.buildUpon().appendPath(location_setting).appendQueryParameter("date", Long.toString(normalizedDate)).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri){
+            return uri.getPathSegments().get(1);
+        }
+
+        public static long getDateFromUri(Uri uri){
+            return Long.parseLong(uri.getPathSegments().get(2));
+        }
+
+        public static long getStartDateFromUri(Uri uri){
+            String dateString = uri.getQueryParameter(COLUMN_DATE);
+            if (null != dateString && dateString.length() > 0)
+                return Long.parseLong(dateString);
+            else
+                return 0;
+        }
+        }
+
+}
---
+++ app/src/main/java/com/example/dbc/sunshine/app/data/WeatherDbHelper.java
@@ -0,0 +1,46 @@
+package com.example.dbc.sunshine.app.data;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.dbc.sunshine.app.data.WeatherContract.LocationEntry;
+import com.example.dbc.sunshine.app.data.WeatherContract.WeatherEntry;
+
+class WeatherDbHelper extends SQLiteOpenHelper{
+
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "weather.db";
+
+    public WeatherDbHelper(Context context){
+        super(context,DATABASE_NAME,null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE" + WeatherEntry.TABLE_NAME + "(" +
+
+             WeatherEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
+             WeatherEntry.COLUMN_LOC_KEY + "INTEGER NOT NULL," +
+             WeatherEntry.COLUMN_WEATHER_ID + "INTEGER NOT NULL," +
+             WeatherEntry.COLUMN_SHORT_DESC + "TEXT NOT NULL," +
+             WeatherEntry.COLUMN_MIN_TEMP + "REAL NOT NULL," +
+             WeatherEntry.COLUMN_MAX_TEMP + "REAL NOT NULL," +
+             WeatherEntry.COLUMN_HUMIDITY + "REAL NOT NULL," +
+             WeatherEntry.COLUMN_PRESSURE + "REAL NOT NULL," +
+             WeatherEntry.COLUMN_WIND_SPEED + "REAL NOT NULL," +
+             WeatherEntry.COLUMN_DEGREES + "REAL NOT NULL," +
+             "FOREIGN KEY ( " + WeatherEntry.COLUMN_LOC_KEY + " ) REFERENCES " +
+             LocationEntry.TABLE_NAME + "( " + LocationEntry._ID + " )," +
+             "UNIQUE (" + WeatherEntry.COLUMN_DATE + "," + WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE;";
+             sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+
+
+    }
+}
